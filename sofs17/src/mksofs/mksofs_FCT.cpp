0083e618796c7020d2a837becb46a163181db0a0
Abastos <antonio.ariel@ua.pt>
2017-10-12 16:51:47 +0100

    Mksofs_FCT beta v1.0

--- sofs17/src/mksofs/mksofs_FCT.cpp
+++ sofs17/src/mksofs/mksofs_FCT.cpp
@@ -10,5 +10,38 @@
 /* see mksofs.h for a description */
 void fillInFreeClusterTable(uint32_t rmstart, uint32_t ctotal)
 {
-    fillInFreeClusterTableBin(rmstart, ctotal);
+	bool original = false;
+	if(original)
+	{
+		fillInFreeClusterTableBin(rmstart,ctotal);
+	}
+	else
+	{
+		uint32_t rpc = (ReferencesPerBlock*BlocksPerCluster)-1;
+
+		uint32_t clusterTable[rpc+1];
+
+		uint32_t cfree = ctotal-1;
+
+		uint32_t ccluster = rmstart+1;
+
+		uint32_t ncluster = cfree;
+
+		for(uint32_t i =1; i<=ctotal; i+=1)
+		{
+			clusterTable = new(clusterTable) uint32_t[rpc+1]();
+			for(uint32_t j = 0; j<rpc ; j+=1)
+			{
+				clusterTable[j] = ncluster ? ccluster++ : NullReference;
+				ncluster -= ncluster ? 1 : 0;
+			}
+
+			clusterTable[rpc]= ncluster ? (i+1) : NullReference;
+
+			soWriteRawBlock(i*BlocksPerCluster+rmstart, clusterTable);
+
+		}
+
+	}
+   
 }
