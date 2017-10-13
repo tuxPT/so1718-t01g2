569f3c4326ce18154f738565a4c4af3964622493
Rodrigo Silva <silvarodrigo@ua.pt>
2017-10-13 16:35:49 +0100

    mksofs_RD.cpp - 1

--- sofs17/src/mksofs/mksofs_RD.cpp
+++ sofs17/src/mksofs/mksofs_RD.cpp
@@ -10,5 +10,35 @@
 /* see mksofs.h for a description */
 void fillInRootDir(uint32_t rtstart)
 {
-    fillInRootDirBin(rtstart);
+	bool original = false;
+	if (original)
+	{
+    	fillInRootDirBin(rtstart);
+    }
+    else
+    {
+    	int size = BlockSize * BlocksPerCluster / sizeof(SODirEntry); // 512 * 4 / 64
+    	SODirEntry arr[size];
+    	
+    	// Fill default entries
+    	arr[0].in = 0;
+    	strncpy(arr[0].name, ".", sizeof(arr[0].name));
+
+    	arr[1].in = 0;
+    	strncpy(arr[1].name, "..", sizeof(arr[0].name));
+
+    	// Fill the rest of the elements as null and zeros
+    	int i;
+    	for (i = 2;i < size; i++)
+    	{
+    		arr[i].in = NullReference;
+    		memset(arr[i].name, 0, sizeof(arr[0].name));
+    	}
+
+    	// Write block by block
+    	for (i = 0; i < 4; i++)
+    	{	
+    		soWriteRawBlock(rtstart + i, &arr[i*size/4]);
+    	}
+    }
 }
