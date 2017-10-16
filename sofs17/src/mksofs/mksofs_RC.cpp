8b0f29aaa6914f1073320cc6a249a7d9e07fbdb0
Fábio Mendes <fabio.jesus.mendes@ua.pt>
2017-10-16 18:55:37 +0100

    working  resetclusters

--- sofs17/src/mksofs/mksofs_RC.cpp
+++ sofs17/src/mksofs/mksofs_RC.cpp
@@ -10,5 +10,16 @@
 /* see mksofs.h for a description */
 void resetClusters(uint32_t cstart, uint32_t ctotal)
 {
-    resetClustersBin(cstart, ctotal);
+
+  //uint8_t tam[BlockSize];
+
+  uint8_t *zeros,tmp;
+  zeros=&tmp;
+  memset(zeros,0,BlockSize);
+  for(uint32_t i=cstart*BlocksPerCluster; i<ctotal*BlocksPerCluster;i++)
+  {
+      soWriteRawBlock(i,zeros);
+  }
+
+    //resetClustersBin(cstart, ctotal);
 }
