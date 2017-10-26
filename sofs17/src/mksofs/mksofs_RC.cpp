151899f0421e11b9535126af28039e5762ef4e76
Fábio Mendes <fabio.jesus.mendes@ua.pt>
2017-10-26 18:02:44 +0100

    resolvido

--- sofs17/src/mksofs/mksofs_RC.cpp
+++ sofs17/src/mksofs/mksofs_RC.cpp
@@ -13,15 +13,15 @@ void resetClusters(uint32_t cstart, uint32_t ctotal)
 
   //uint8_t tam[BlockSize];
 
-  uint8_t *zeros,tmp;
-//  uint8_t *zeros =(uint8_t*)calloc (512,sizeof(uint8_t));
-  zeros=&tmp;
+  //uint8_t *zeros,tmp;
+ uint8_t *zeros =(uint8_t*)calloc (512,sizeof(uint8_t));
+//  zeros=&tmp;
 
-//  memset(zeros,0,BlockSize);
-for(uint32_t j=0;j<BlockSize;j++)
-{
-  zeros[j]=0;
-}
+////  memset(zeros,0,BlockSize);
+//for(uint32_t j=0;j<BlockSize;j++)
+//{
+  //zeros[j]=0;
+//}
 
   for(uint32_t i=cstart*BlocksPerCluster; i<ctotal*BlocksPerCluster;i++)
   {
