0ad221b8b91cfed2839709bb80d3eb2f464b11d6
Rodrigo Silva <silvarodrigo@ua.pt>
2017-11-17 20:58:58 +0000

    Fixed free_inode.cpp and completed free_fileclusters.cpp

--- sofs17/src/ilayers/fileclusters/free_fileclusters.cpp
+++ sofs17/src/ilayers/fileclusters/free_fileclusters.cpp
@@ -1,51 +1,177 @@
 /**
- *  \author ...
- *  \tester ...
+ *  \author Rodrigo Cunha e Silva
+ *  \tester Rodrigo Cunha e Silva
  */
 
 #include "fileclusters.h"
 #include "fileclusters.bin.h"
+#include "freelists.h"
 
 #include "probing.h"
 #include "exception.h"
 #include "datatypes.h"
 
+#include "itdealer.h"
+#include "sbdealer.h"
+#include "czdealer.h"
+
 #include <errno.h>
 #include <stdint.h>
 
-#if 0
-static void soFreeIndirectFileClusters(SOInode * ip, uint32_t ffcn);
-static void soFreeDoubleIndirectFileClusters(SOInode * ip, uint32_t ffcn);
+#include <iostream>
+
+#if 1
+static void soFreeIndirectFileClusters(uint32_t i1, SOInode * ip, uint32_t ffcn, int * total);
+static void soFreeDoubleIndirectFileClusters(SOInode * ip, uint32_t ffcn, int * total);
 #endif
 
-/* ********************************************************* */
+using namespace std;
 
+/* ********************************************************* */
+/// Note: I think there's an edge case I couldn't figure out and therefore isn't tested
+/// Se alguém tiver erro aqui ^ que avise!
 void soFreeFileClusters(int ih, uint32_t ffcn)
 {
     soProbe(402, "soFreeFileClusters(%d, %u)\n", ih, ffcn);
-    soFreeFileClustersBin(ih, ffcn);
+
+    // Get super block pointer
+    SOSuperBlock * sbp = sbGetPointer();  
+
+    // Get node pointer
+	SOInode * node = iGetPointer(ih);
+
+    // Get total number of cluster occupied
+    int total = node->clucnt;
+
+    unsigned int i;
+
+    // printf("DEBUG: ffcn: %d Total : %d\n", ffcn, total);
+
+    // If only .d is occupied
+    if (ffcn < N_DIRECT)
+    {
+        for (i = 1; i <= N_DIRECT; i++)
+        {
+            if (node->d[i-1] != NullReference)
+            {
+                // printf("DEBUG: FOR: %d\n", i);
+                soFreeCluster(i);
+                node->d[i-1] = NullReference;
+                total--;
+            }
+        }
+
+        // Stop if no more clusters to clear
+        if (total > 0)
+        {
+            // Process i1
+            soFreeIndirectFileClusters(node->i1, node, 0, &total);
+
+            // Stop if no more clusters to clear
+            if (total > 0)
+            {
+                // Process i2
+                soFreeDoubleIndirectFileClusters(node, 0, &total);   
+            }
+        }
+    }
+    else if (ffcn > 5 && ffcn < 518)
+    {
+        // printf("DEBUG (INDIRECT): %d \n", (int)(ffcn - N_DIRECT));
+        soFreeIndirectFileClusters(node->i1, node, ffcn - N_DIRECT, &total);
+        soFreeDoubleIndirectFileClusters(node, 0, &total);
+    }
+    else
+    {
+        // printf("DEBUG (INDIRECT): %d \n", (int)(ffcn - N_DIRECT - ReferencesPerBlock));
+        soFreeDoubleIndirectFileClusters(node, ffcn - N_DIRECT - ReferencesPerBlock, &total);
+    }
+
+    // Update cluster count
+    node->clucnt = total;
+
+    // printf("DEBUG: AFTER TOTAL: %d\n", total);
+
+    iSave(ih);
+    sbSave();
 }
 
-#if 0
+#if 1
 /* ********************************************************* */
 
 /* only a hint to decompose the solution */
-static void soFreeIndirectFileClusters(SOInode * ip, uint32_t ffcn)
+static void soFreeIndirectFileClusters(uint32_t i1, SOInode * ip, uint32_t ffcn, int * total)
 {
     soProbe(402, "soFreeIndirectFileClusters(%p, %u)\n", ip, ffcn);
 
-    throw SOException(ENOSYS, __FUNCTION__);
+    if (i1 == NullReference)
+    {
+        // printf("DEBUG: I1 IS NULL!!!\n");
+        return;
+    }
+
+    // Alloc size for references
+    uint32_t * refs = (uint32_t*) malloc(ReferencesPerBlock * sizeof(uint32_t));
+
+    // Read cluster i1 (may be i1 or a reference from i2)
+    soReadCluster(i1, refs);
+
+    unsigned int i;
+
+    // Iterate cluster references
+    for (i = ffcn; i < ReferencesPerBlock; i++)
+    {
+        if (refs[i] != NullReference)
+        {            
+            // Decrement cluster count and free cluster
+            // printf("DEBUG: INDIRECT: CLUSTER %d cleared!\n", refs[i]);
+            soFreeCluster(refs[i]);
+            refs[i] = NullReference;
+            (*total)--;
+            
+        }
+    }
+
+    // Free own cluster
+    soFreeCluster(i1);
+    ip->i1 = NullReference;
+    (*total)--;
 }
 
 /* ********************************************************* */
 
 /* only a hint to decompose the solution */
-static void soFreeDoubleIndirectFileClusters(SOInode * ip, uint32_t ffcn)
-{
+static void soFreeDoubleIndirectFileClusters(SOInode * ip, uint32_t ffcn, int * total)
+{    
     soProbe(402, "soFreeDoubleIndirectFileClusters(%p, %u)\n", ip, ffcn);
+    // printf("DEBUG: DOUBLE arrived\n");
 
-    throw SOException(ENOSYS, __FUNCTION__);
+    if (ip->i2 == NullReference)
+    {
+        return;
+    }
+
+    uint32_t * refs = (uint32_t*) malloc(ReferencesPerBlock * sizeof(uint32_t));
+    soReadCluster(ip->i2, refs);
+
+    unsigned int i;
+
+    for (i = ffcn; i < ReferencesPerBlock; i++)
+    {
+        if (refs[i] != NullReference)
+        {
+            uint32_t c_refs = refs[i];
+            // printf("DEBUG: DOUBLE: c_refs: %d ffcn: %d \n", c_refs, ffcn);
+            soFreeIndirectFileClusters(c_refs, ip, 0, total); // initial location should be verified
+        }
+    }
+
+    // Free own cluster
+    soFreeCluster(ip->i2);
+    ip->i2 = NullReference;
+    (*total)--;
 }
+
 #endif
 
 /* ********************************************************* */
--- sofs17/src/ilayers/freelists/free_inode.cpp
+++ sofs17/src/ilayers/freelists/free_inode.cpp
@@ -29,54 +29,79 @@ void soFreeInode(uint32_t in)
 	soFreeInodeBin(in);
 #else
 		soOpenSBDealer();
-		// Read i-node
-		uint32_t ih = iOpen(in);
-		iCheckHandler(ih);
 
-		SOInode * node = iGetPointer(ih);
+		// Read i-node
+		uint32_t node_handler = iOpen(in);
+		iCheckHandler(node_handler);
+
+		// Get pointer to node
+		SOInode * node = iGetPointer(node_handler);
 
 		// Apply bit mask
 		node->mode = node->mode | INODE_FREE;
 
 		// Read SB
-		SOSuperBlock * sb = sbGetPointer();
+		SOSuperBlock * sbp = sbGetPointer();
 
-		// Get head node
-		int hhead = iOpen(sb->ihead);
-		SOInode * headNode = iGetPointer(hhead);
-
-		if (sb->ifree == 0)
+		// If free inode list is empty
+		if (sbp->ifree == 0)
 		{
-			sb->ihead = in;
+			sbp->ihead = in;
 			node->prev = in;
 			node->next = in;
 		}
-		else if (sb->ifree == 1)
+		// If it has one element
+		else if (sbp->ifree == 1)
 		{
-			headNode->prev = in;
-			headNode->next = in;
+			// Get head node
+			int head_handler = iOpen(sbp->ihead);
+			SOInode * head_node = iGetPointer(head_handler);
 
-			node->next = sb->ihead;
-			node->prev = sb->ihead;
-			iSave(hhead);
+			// Set head's previous and next node as input node
+			head_node->prev = in;
+			head_node->next = in;
+
+			// Reciprocally, set previous and next of input node as head's number
+			node->next = sbp->ihead;
+			node->prev = sbp->ihead;
+
+			// Save inodes
+			iSave(head_handler);
+			iSave(node_handler);
 		}
 		else
 		{
-			int hprev = iOpen(headNode->prev);
-			SOInode * previousNode = iGetPointer(hprev);
-			previousNode->next = in;
-			node->prev = headNode->prev;
-			headNode->prev = in;
-			node->next = sb->ihead;
+			// Get head node
+			int head_handler = iOpen(sbp->ihead);
+			SOInode * head_node = iGetPointer(head_handler);
 
-			iSave(hhead);
-			iSave(hprev);
+			// Get tail node
+
+			int tail_handler = iOpen(head_node->prev);
+			SOInode * tail_node = iGetPointer(tail_handler);
+
+			// Set tail's next node as input node
+			tail_node->next = in;
+
+			// Set input node next as current head
+			node->next = sbp->ihead;
+
+			// Set previous as current tail
+			node->prev = head_node->prev;
+
+			// Set head's previous as input node
+			head_node->prev = in;
+
+			// TAIL <-> HEAD ===> TAIL <-> NEW <-> HEAD
+
+			iSave(head_handler);
+			iSave(tail_handler);
 		}
 
-		sb->ifree++;
+		sbp->ifree++;
 
-		iSave(hhead);
-		soCloseSBDealer();
-		iClose(ih);
+		sbSave();
+		iSave(node_handler);
+		iClose(node_handler);
 #endif
 }
