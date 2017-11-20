792b124cc62351ee148d319c74ce43e622cd3684
Ricardo Martins <ferreira.martins@ua.pt>
2017-11-20 11:35:53 +0000

    untestd unchecked

--- sofs17/src/ilayers/fileclusters/write_filecluster.cpp
+++ sofs17/src/ilayers/fileclusters/write_filecluster.cpp
@@ -8,6 +8,8 @@
 
 #include "probing.h"
 #include "exception.h"
+#include "czdealer.h"
+#include "datatypes.h"
 
 #include <errno.h>
 #include <inttypes.h>
@@ -15,5 +17,8 @@
 void soWriteFileCluster(int ih, uint32_t fcn, void *buf)
 {
     soProbe(405, "soWriteFileCluster(%d, %u, %p)\n", ih, fcn, buf);
-    soWriteFileClusterBin(ih, fcn, buf);
+ //   soWriteFileClusterBin(ih, fcn, buf);
+	uint32_t cluster_num=soGetFileCluster(ih,fcn);
+	if(cluster_num==NullReference)	cluster_num=soAllocFileCluster(ih,fcn);
+	soWriteCluster(cluster_num,buf);
 }
